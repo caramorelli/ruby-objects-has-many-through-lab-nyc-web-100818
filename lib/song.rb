9449f11e658e68d441bfa4e1c3de108c06816c6f
Cara Morelli <caramorelli7@gmail.com>
2018-10-12 00:42:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,43 @@
-# class Song 
+class Song 
   
-#   attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
   
-#   @@all = []
-  
-#   def initialize(name, artist, genre)
-#     @name = name 
-#     @artist = artist 
-#     @genre = genre
-#     @@all << self
-#     genre.songs << self 
-#     artist.songs << self
-#   end 
-  
-#   def self.all 
-#     @@all
-#   end
-
-
-# end 
-
-class Song
-
-  attr_accessor :name, :genre, :artist
-
   @@all = []
-
+  
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
+    @name = name 
+    @artist = artist 
     @genre = genre
-    genre.songs << self
-    artist.songs << self
     @@all << self
-  end
-
-  def self.all
+    genre.songs << self 
+    artist.songs << self
+  end 
+  
+  def self.all 
     @@all
   end
 
 
-end
+# end 
+
+# class Song
+
+#   attr_accessor :name, :genre, :artist
+
+#   @@all = []
+
+#   def initialize(name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     genre.songs << self
+#     artist.songs << self
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
+
+
+# end
